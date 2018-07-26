664d64f4d4a89ae48fbb970d5211858599c77fd4
zoeviali <z.viali@bluewin.ch>
2018-07-26 15:15:31 +0200

    MAJ

---
+++ Assets/Scenes/Scriptesoucoupe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptesoucoupe : MonoBehaviour {
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position+new Vector3(0.06f, 0, 0);
+	}
+}
