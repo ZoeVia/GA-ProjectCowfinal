4ff300e75a99ae2582854c2e4a8210e7cbb51059
zoeviali <z.viali@bluewin.ch>
2018-07-26 12:23:21 +0200

    Ecran fin du jeu

--- Assets/Scenes/PlayerInput.cs
+++ Assets/Scenes/PlayerInput.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInput : MonoBehaviour
 {
     public float playerVelocity = 10f;
     private Rigidbody2D rigidbody2D;
+    public Camera camera;
+    
 
 	// Use this for initialization
 	void Start ()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+
 	}
 
     // Update is called once per frame
     void Update()
+
     {
+        camera.transform.position = transform.position-new Vector3(0,0,10);
+        rigidbody2D.transform.position += new Vector3(0.06f,0,0);
         /*
         float horizontalAxis = Input.GetAxis("Horizontal");
         Vector2 velocity =
@@ -31,6 +38,18 @@ public class PlayerInput : MonoBehaviour
         {
             rigidbody2D.gravityScale *=- 1;
             GetComponent<SpriteRenderer>().flipY = !GetComponent<SpriteRenderer>().flipY;
+        }
+        
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+            // trigger fin du jeu
+            SceneManager.LoadScene("FinDuJeu");
         }
     }
 }
